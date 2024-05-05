dd9a2ef27d98d12730a93eab1d674f0f72ace69e
Javier González Falagán <160909746+jjaviergonzalezfalagan@users.noreply.github.com>
2024-05-05 12:11:54 +0200

    Add files via upload

---
+++ docs/js/script.js
@@ -0,0 +1,15 @@
+/* 
+ *  Ciclo Formativo de Grao Superior en Desenvolvemento de Aplicacións Web
+ *  Deseño de Interfaces Web
+ *  J.Javier González Falagán
+ *  ue47292@edu.xunta.gal
+ *  7 de marzo de 2024
+ */
+
+function enviarFormulario(){
+	alert("\nSus datos han sido enviados correctamente. \n \nGracias por ponerse en contacto con nosotros.");
+}
+
+function enviarSuscripcion(){
+	alert("\nSus datos han sido enviados correctamente. \n \nGracias por suscribirse a nuestro boletín de noticias.");
+}
